f852ca9f74932280e3ca2d7b125015469a2bbf40
Gal Aharon <gal.aharon99@gmail.com>
2014-12-07 12:24:22 +1100

    Update build.gradle and lib version.

--- src/main/java/com/shieldbug1/core/internal/handler/InternalServerEventHandler.java
+++ src/main/java/com/shieldbug1/core/internal/handler/InternalServerEventHandler.java
@@ -3,8 +3,6 @@ package com.shieldbug1.core.internal.handler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import com.shieldbug1.lib.util.ServerUtils;
-
 public final class InternalServerEventHandler
 {
 	public static final InternalServerEventHandler INSTANCE = new InternalServerEventHandler();
@@ -12,13 +10,6 @@ public final class InternalServerEventHandler
 	@SubscribeEvent
 	public void onServerTick(TickEvent.ServerTickEvent event)
 	{
-		if(event.phase.equals(TickEvent.Phase.START))
-		{
-			ServerUtils.preTick();
-		}
-		else if(event.phase.equals(TickEvent.Phase.END))
-		{
-			ServerUtils.postTick();
-		}
+		
 	}
 }
